8717597f503d789489a3108c716023ec1112c694
Chaitanya360 <chaitanyajadhav360@gmail.com>
2021-10-08 11:12:53 +0530

    change size of logo

--- src/components/GothicLogo.js
+++ src/components/GothicLogo.js
@@ -5,39 +5,55 @@ const styles = {
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
-    width: "fit-content",
-    heigth: "fit-content",
     position: "absolute",
     top: "1rem",
     left: "1rem",
     borderRadius: "5px",
     overflow: "hidden",
+    width: "90px",
+    boxShadow: "1px 1px 2px 1px rgba(0,0,0,0.3)",
   },
   img: {
-    width: "150px",
-    heigth: "150px",
-    objectFit: "containe",
+    width: "100%",
+    heigth: "auto",
+    objectFit: "cover",
     cursor: "pointer",
-    borderRadius: "5px",
-    border: "2px solid transparent",
-    boxShadow: "1px 1px 2px 1px rgba(0,0,0,0.3)",
+    borderRadius: "5px 5px 0 0",
+  },
+
+  text: {
+    color: "white",
+    fontSize: "0.8rem",
+    fontWeight: "300",
+    width: "100%",
+    textAlign: "center",
+    backgroundColor: "tomato",
+    padding: "2px 5px",
+  },
+  link: {
+    display: "flex",
+    flexDirection: "column",
+    margin: 0,
+    padding: 0,
   },
 };
 
 function GothicLogo(props) {
   return (
-    <div style={styles.body}>
+    <div style={styles.body} className="gothic-logo">
       <a
         href="https://www.gothichomes.in/"
         target="_blank"
         rel="noopener noreferrer"
+        style={styles.link}
       >
         <img
           src={`${process.env.PUBLIC_URL}/images/icons/logo.png`}
           alt="gothic-logo"
           style={styles.img}
-          className="gothic-logo"
+          title="visit site"
         />
+        <div style={styles.text}>visit website</div>
       </a>
     </div>
   );
